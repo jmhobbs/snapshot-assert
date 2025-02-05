bdb76373ab03fcd042176374a2835f415944eb67
John Hobbs <john@velvetcache.org>
2025-02-04 22:08:48 -0600

    Fix interface on default methods

--- default.go
+++ default.go
@@ -7,17 +7,17 @@ import (
 
 var Default *Snapshots = New()
 
-func Assert(t *testing.T, actual image.Image) {
+func Assert(t testing.TB, actual image.Image) {
 	t.Helper()
 	Default.Assert(t, actual)
 }
 
-func Fail(t *testing.T, actual image.Image) {
+func Fail(t testing.TB, actual image.Image) {
 	t.Helper()
 	Default.Fail(t, actual)
 }
 
-func Test(t *testing.T, actual image.Image) error {
+func Test(t testing.TB, actual image.Image) error {
 	t.Helper()
 	return Default.Test(t, actual)
 }
